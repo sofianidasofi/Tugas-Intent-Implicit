0d1e4bbb0c3e9f7bf2bb6a75dc184b7101fbbcc8
sofianidasofi <sofianida29@gmail.com>
2019-10-01 22:15:36 +0700

    Tugas1

---
+++ app/src/main/java/com/example/implicit/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.implicit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void panggil(View view){
+        String nomor = "082262674686";
+        Intent panggil = new Intent(Intent. ACTION_DIAL);
+        panggil.setData(Uri. fromParts("tel",nomor,null));
+        startActivity(panggil);
+    }
+
+    public void buka(View view){
+        String url = "https://badoystudio.com";
+        Intent bukabrowser = new Intent(Intent. ACTION_VIEW);
+        bukabrowser.setData(Uri. parse(url));
+        startActivity(bukabrowser);
+    }
+}
